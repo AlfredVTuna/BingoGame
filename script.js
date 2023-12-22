d9ff727b8e7b17fbb83ed3f5423a55d8b00017fa
AlfredVTuna <152959093+AlfredVTuna@users.noreply.github.com>
2023-12-22 08:57:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var bingoWords = [
 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,
 18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,
 36,37,38,39,40,41,42,43,44,45,46,47,48,49,50  // all the way to 50
-
 ];
 
 function shuffle(array) {
@@ -77,13 +76,13 @@ function generateBingoBoard() {
                 cell.textContent = "Biljett till ecsape room";
                 row.appendChild(cell);
                 }
-                else if(j==0 && i=2){
+                else if(j==0 && i==2){
                 var cell = document.createElement("td");
                 cell.className = "bingo-row";
                 cell.textContent = "Trofe";
                 row.appendChild(cell);
                 }
-                    else if(j==0 && i=3){
+                    else if(j==0 && i==3){
                 var cell = document.createElement("td");
                 cell.className = "bingo-row";
                 cell.textContent = "15min i bollhavet";
