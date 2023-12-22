3781c95a8cb9c68837a3ec2b073188ae41fc1707
AlfredVTuna <152959093+AlfredVTuna@users.noreply.github.com>
2023-12-22 08:40:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@
 var bingoWords = [
 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,
 18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,
-36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,
-61,62,63,64,65,66,67,68,69,70,71,72,73,74,75  // all the way to 75
+36,37,38,39,40,41,42,43,44,45,46,47,48,49,50  // all the way to 50
 
 ];
 
@@ -66,10 +65,30 @@ function generateBingoBoard() {
             }
             
             if(j==0){
+                if(i=0){
+                var cell = document.createElement("td");
+                cell.className = "bingo-row";
+                cell.textContent = "Bild";
+                row.appendChild(cell);
+                }
+                else if(i=1){
+                var cell = document.createElement("td");
+                cell.className = "bingo-row";
+                cell.textContent = "Biljett till ecsape room";
+                row.appendChild(cell);
+                }
+                else if(i=2){
+                var cell = document.createElement("td");
+                cell.className = "bingo-row";
+                cell.textContent = "Trofe";
+                row.appendChild(cell);
+                }
+                else {
                 var cell = document.createElement("td");
                 cell.className = "bingo-row";
                 cell.textContent = "Pris";
                 row.appendChild(cell);
+                }
             }
             if(i != 0 && j != 0){
             var cell = document.createElement("td");
